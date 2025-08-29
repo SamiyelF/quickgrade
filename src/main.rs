6be435c8c171cfac1bc0cd7864c6920c2ba2fef1
Samiyel Frazier <samiyelfrazier@gmail.com>
2025-08-28 20:04:29 -0700

    pass/fail, add ratatui to deps

--- src/main.rs
+++ src/main.rs
@@ -8,37 +8,34 @@ use std::sync::Arc;
 use std::thread;
 #[derive(Copy, Clone)]
 struct Grade {
-    val: Option<f32>,
+    val: Option<bool>,
 }
 impl Grade {
-    fn normalize(&mut self) {
-        self.val = match self.val {
-            Some(v) => Some(v.clamp(0.0, 1.0)),
-            None => None,
-        };
-    }
-    fn get(&self) -> f32 {
+    fn get(&self) -> bool {
         match self.val {
             Some(v) => v,
-            _ => 0.0,
+            _ => false,
         }
+    }
+    fn perc(&self) -> f32 {
+        if self.get() { 1.0 } else { 0.0 }
     }
     fn fail(&mut self) {
         self.val = match self.val {
             Some(_) => self.val,
-            None => Some(0.0),
+            None => Some(false),
         };
     }
     fn pass(&mut self) {
         self.val = match self.val {
             Some(_) => self.val,
-            None => Some(1.0),
+            None => Some(true),
         };
     }
     fn empty() -> Grade {
         Grade { val: None }
     }
-    fn new(v: f32) -> Grade {
+    fn new(v: bool) -> Grade {
         Grade { val: Some(v) }
     }
 }
@@ -115,16 +112,12 @@ struct Rubric {
     ques: Grade,
 }
 impl Rubric {
-    fn normalize(&mut self) {
-        self.link.normalize();
-        self.caps.normalize();
-        self.punc.normalize();
-        self.spel.normalize();
-        self.ques.normalize();
-    }
     fn get(&mut self) -> f32 {
-        self.normalize();
-        (self.link.get() + self.caps.get() + self.punc.get() + self.spel.get() + self.ques.get())
+        (self.link.perc()
+            + self.caps.perc()
+            + self.punc.perc()
+            + self.spel.perc()
+            + self.ques.perc())
             / 5.0
     }
     fn new() -> Rubric {
@@ -137,7 +130,7 @@ impl Rubric {
         }
     }
     fn from_string(contents: String) -> Rubric {
-        fn punc_spell_caps(contents: &String) -> (f32, f32, f32) {
+        fn punc_spell_caps(contents: &String) -> (bool, bool, bool) {
             let lints = bucket_lints(contents);
             let mut punc = Grade::empty();
             let mut spel = Grade::empty();
@@ -158,14 +151,11 @@ impl Rubric {
             let regex = Regex::new(r"((youtube.com)|(youtu.be)|(tiktok.com))\/").unwrap();
             regex.is_match(contents)
         }
+
         let mut out = Rubric::new();
         let contents_clone = contents.clone();
         let handle = thread::spawn(move || punc_spell_caps(&contents_clone));
-        out.link = if contains_link(&contents) {
-            Grade::new(1.0)
-        } else {
-            Grade::new(0.0)
-        };
+        out.link = Grade::new(contains_link(&contents));
         println!("{}", contents);
         println!("Complete sentences and all questions answered?");
         let mut input = String::new();
@@ -190,22 +180,22 @@ impl Rubric {
         let mut out = String::new();
         out += &format!(
             "{}%(20%): Contains a link to a youtube video\n",
-            self.link.get() * 20.0
+            self.link.perc() * 20.0
         )
         .to_string();
-        out += &format!("{}%(20%): No spelling mistakes\n", self.spel.get() * 20.0);
+        out += &format!("{}%(20%): No spelling mistakes\n", self.spel.perc() * 20.0);
         out += &format!(
             "{}%(20%): No punctuation mistakes\n",
-            self.punc.get() * 20.0
+            self.punc.perc() * 20.0
         );
         out += &format!(
             "{}%(20%): No capitalization mistakes\n",
-            self.caps.get() * 20.0
+            self.caps.perc() * 20.0
         )
         .to_string();
         out += &format!(
             "{}%(20%): Answered all the questions in complete sentences\n",
-            self.ques.get() * 20.0
+            self.ques.perc() * 20.0
         )
         .to_string();
         out += &"#== === === === =#= === === === ==#\n".to_string();
